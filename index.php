663898b19db6ff943e5f66e42e396da2ea7f5912
elizaveta-mikka <102608234+elizaveta-mikka@users.noreply.github.com>
2022-06-08 01:46:55 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Лабораторные работы</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<h1>Лабораторные работы</h1>
+<a href="mldnlab1.php">Первая лабораторная работа по МЛиДМ</a>
+<br>
+<a href="mldnlab2.php">Вторая лабораторная работа по МЛиДМ</a>
+<br>
+<a href="mldnlab3.php">Третья лабораторная работа по МЛиДМ</a>
+<br>
+<a href="mldnlab4.php">Четвертая лабораторная работа по МЛиДМ</a>
+<br>
+<a href="mldnlab5.php">Пятая лабораторная работа по МЛиДМ</a>
+</body>
+</html>
